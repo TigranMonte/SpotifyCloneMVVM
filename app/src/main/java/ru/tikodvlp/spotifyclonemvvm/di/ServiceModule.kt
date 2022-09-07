ee4039134bd3e71b4498b101a9c246599f50dee3
Tigran <tigranma@gmail.com>
2022-09-07 21:41:08 +0300

    Lesson-6

--- app/src/main/java/ru/tikodvlp/spotifyclonemvvm/di/ServiceModule.kt
+++ app/src/main/java/ru/tikodvlp/spotifyclonemvvm/di/ServiceModule.kt
@@ -12,10 +12,15 @@ import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ServiceComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ServiceScoped
+import ru.tikodvlp.spotifyclonemvvm.data.remote.MusicDatabase
 
 @Module
 @InstallIn(ServiceComponent::class)
 object ServiceModule {
+
+    @ServiceScoped
+    @Provides
+    fun provideMusicDatabase() = MusicDatabase()
 
     @ServiceScoped
     @Provides
