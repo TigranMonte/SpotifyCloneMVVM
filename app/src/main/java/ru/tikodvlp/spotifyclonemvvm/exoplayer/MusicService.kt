cc7ddd60aedd170d2d5fc3fe0fb3da53c4d79dd4
Tigran <tigranma@gmail.com>
2022-09-08 17:58:48 +0300

    Lesson-6 Update

--- app/src/main/java/ru/tikodvlp/spotifyclonemvvm/exoplayer/MusicService.kt
+++ app/src/main/java/ru/tikodvlp/spotifyclonemvvm/exoplayer/MusicService.kt
@@ -13,6 +13,7 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
 import kotlinx.coroutines.cancel
+import ru.tikodvlp.spotifyclonemvvm.exoplayer.callbacks.MusicPlayerNotificationListener
 import javax.inject.Inject
 
 private const val SERVICE_TAG = "MusicService"
@@ -26,11 +27,15 @@ class MusicService : MediaBrowserServiceCompat() {
     @Inject
     lateinit var exoPlayer: ExoPlayer
 
+    private lateinit var musicNotificationManager: MusicNotificationManager
+
     private val serviceJob = Job()
     private val serviceScope = CoroutineScope(Dispatchers.Main + serviceJob)
 
     private lateinit var mediaSession: MediaSessionCompat
     private lateinit var mediaSessionConnector: MediaSessionConnector
+
+    var isForegroundService = false
 
     override fun onCreate() {
         super.onCreate()
@@ -44,6 +49,14 @@ class MusicService : MediaBrowserServiceCompat() {
         }
 
         sessionToken = mediaSession.sessionToken
+
+        musicNotificationManager = MusicNotificationManager(
+            this,
+            mediaSession.sessionToken,
+            MusicPlayerNotificationListener(this)
+        ) {
+
+        }
 
         mediaSessionConnector = MediaSessionConnector(mediaSession)
         mediaSessionConnector.setPlayer(exoPlayer)
--- app/src/main/java/ru/tikodvlp/spotifyclonemvvm/other/Constants.kt
+++ app/src/main/java/ru/tikodvlp/spotifyclonemvvm/other/Constants.kt
@@ -3,4 +3,7 @@ package ru.tikodvlp.spotifyclonemvvm.other
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+
+    const val NOTIFICATION_CHANNEL_ID = "music"
+    const val NOTIFICATION_ID = 1
 }
